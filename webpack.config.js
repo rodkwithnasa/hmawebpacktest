b297e5e816dab5c759a744adc18b998af1f0208b
Howard Atkins <atkins.howard@gmail.com>
2019-11-29 20:56:47 +0000

    add font

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
            'file-loader',
          ],
        },
-
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader',
+         ],
+       },
      ],
    },
 };
