3430e4f1289b42ce878e73e3466487eb196ee562
rodkwithnasa <34109135+rodkwithnasa@users.noreply.github.com>
2026-03-01 17:13:15 +0000

    updated to remove dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import _ from 'lodash';
 import printMe from './print.js';
+import htmlUrl from '../index.html';
+
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = ['Hello', 'webpack'].join(' ');
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
+  console.log("HTML is available at:",htmlUrl); 
 
   element.appendChild(btn);
   
